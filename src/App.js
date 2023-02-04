1702e261e044da05e88b52d1383dc2c4dbce4ffe
Paul Nix <kenpopaul@yahoo.co.uk>
2023-02-04 06:43:45 +0000

    Tailwinds css error fixed & appliued styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Board from "./components/board";
 import Input from "./components/Input";
 
 function App() {
@@ -8,8 +9,18 @@ console.log(taskList);
 
   return (
     <>
-    <h1>To Do Board....</h1>
-    <Input taskList={taskList} setTaskList={setTaskList}/>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-xl font-semibold">To Do Board....</h1>
+      <Input taskList={taskList} setTaskList={setTaskList}/>
+      </div>
+      <div className="grid grid-cols-3">
+        {taskList.map((task, index)=>
+        <Board 
+          key={index}
+          task={task}
+        />
+        )}
+    </div>
     </>
   );
 }
---
+++ src/components/board.jsx
@@ -0,0 +1,11 @@
+const Board = ({task}) => {
+    return(
+        <>  
+            <div className="flex flex-col items-center justify-center flexborder">
+                <p>{task}</p>
+            </div>
+        </>
+    )
+}
+
+export default Board; 
